caa7478509d8f977fa07b45f36aff714450c4ae5
Jobizil <65118336+jobizil@users.noreply.github.com>
2020-10-11 14:50:39 +0100

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,14 +4,14 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
   //
-  // if (error.name === "CastError") {
-  //   const message = `Restaurant with such Id not found.`;
-  //   error = new ErrorResponse(message, 404);
-
   if (error.name === "CastError") {
-    const message = `No such Id found.`;
+    const message = `Restaurant with such Id not found.`;
     error = new ErrorResponse(message, 404);
-  }
+
+//   if (error.name === "CastError") {
+//     const message = `No such Id found.`;
+//     error = new ErrorResponse(message, 404);
+//   }
   // Handles Mongoose bad ObjectId
   if (error.code === 11000) {
     const message = "Oops, duplicate value entered";
